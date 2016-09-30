c27747d782ea56e0eee8faf12cdfd92de8b89e96
RangelZZZ <18792561236@163.com>
2016-09-30 12:41:06 +0800

    react can work

---
+++ js/reflux.js
@@ -0,0 +1,11 @@
+'use strict';
+
+class App extends React.Component {
+    render() {
+        return <div>
+            helloWord!
+        </div>
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById("content"));
