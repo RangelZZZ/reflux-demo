f7f7fc396dd8fde3a73b22d6bdcf6d54c59798ca
RangelZZZ <18792561236@163.com>
2016-10-06 14:50:23 +0800

    finished reflux-text

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-    entry: './js/reflux.js',
+    entry: './js/reflux-text.js',
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'bundle.js'
