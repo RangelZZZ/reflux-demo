cbb7827f439f80ae1064555805f9d3717ba2f2a3
rangel <18792561236@163.com>
2016-10-17 14:00:55 +0800

    reflux demo

---
+++ README.md
@@ -0,0 +1,14 @@
+# reflux-demo
+###克隆该项目至本地
+git clone https://github.com/RangelZZZ/reflux-demo
+###执行第一个 Demo（Reflux+React 实现页面渲染hello world！）
+* 执行 npm install
+* 执行npm run webpack
+* 运行hello.html
+
+###执行第二个 Demo（Reflux+React 实现页面显示用户输入的文本内容）
+* 运行index.html
+
+###执行第二个 Demo（Reflux+React+Express 实现页面渲染hello world！）
+git * 执行npm start
+* 访问 localhost:3000
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+
+    entry: {
+         page1:'./js/reflux.js',
+         page2:'./js/reflux-text.js',
+         page3:'./js/reflux-api.js'
+
+    },
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: '[name].bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            { test: /\.js|jsx$/, loaders: ['babel'] }
+        ]
+    }
+};
